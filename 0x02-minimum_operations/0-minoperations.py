02bab19b190b991d19ed6bd14033130b60f20a25
Edimatic <ediomiudofia@gmail.com>
2024-08-08 11:22:18 +0000

    Minimum Operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
+
 """
-This module contains a function that calculates the minimum
-number of operations needed to result in exactly n "H" characters
-in a file.
+    Method that determines the number of minmum operations given n characters
 """
+
 
 def minOperations(n):
-    """Calculate the minimum number of operations to get n H characters."""
-    if n <= 1:
-        return 0
+    """
+        A function that calculates the fewest number of operations
+        needed to give a result of exactly n H characters in a file
+        args: n: Number of characters to be displayed
+        return:
+               number of min operations
+    """
 
-    operations = 0
-    divisor = 2
-
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
-
-    return operations
-
+    now = 1
+    start = 0
+    counter = 0
+    while now < n:
+        remainder = n - now
+        if (remainder % now == 0):
+            start = now
+            now += start
+            counter += 2
+        else:
+            now += start
+            counter += 1
+    return counter
